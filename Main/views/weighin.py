a0d6ae1afcc3a0d0abaa78c626f4be2942950552
Jack Ramey <jack.ramey@gmail.com>
2013-11-08 21:17:02 -0800

    Adding forms.py and weighin pages.

---
+++ Main/views/weighin.py
@@ -0,0 +1,21 @@
+from ..util import session
+from ..models import *
+from ..forms import *
+from flask.ext.login import current_user
+from flask import render_template, request, url_for
+import flask.views
+
+class WeighInView(flask.views.View):
+    methods = ['GET', 'POST']
+
+    def dispatch_request(self):
+        form = WeighInForm(request.form)
+        weighins = WeighIn.query.filter_by(user_id=current_user.id)
+        if request.method == 'POST' and form.validate_on_submit():
+            weigh_in = WeighIn(form.weight.data, current_user.id, form.date.data)
+            db.session.add(weigh_in)
+            db.session.commit()
+            return render_template('weigh_in.html', form=form, weighins=weighins)
+
+        return render_template('weigh_in.html', form=form, weighings=weighins)
+
